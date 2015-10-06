5c9c292a961d8eac61b20b80c5941cbc7db13e59
amandamaturo <amanda.maturo@quinnipiac.edu>
2015-10-06 18:59:20 +0000

    Add user Authorization

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render
 
 from django.views.generic import TemplateView
@@ -45,12 +46,24 @@ class QuestionUpdateView(UpdateView):
   template_name = 'question/question_form.html'
   fields = ['title', 'description']
 
+  def get_object(self, *args, **kwargs):
+    object = super(QuestionUpdateView, self).get_object(*args, **kwargs)
+    if object.user != self.request.user:
+      raise PermissionDenied()
+    return object
+
 from django.views.generic import DeleteView
 
 class QuestionDeleteView(DeleteView):
   model = Question
   template_name = 'question/question_confirm_delete.html'
   success_url = reverse_lazy('question_list')
+
+  def get_object(self, *args, **kwargs):
+    object = super(QuestionDeleteView, self).get_object(*args, **kwargs)
+    if object.user != self.request.user:
+      raise PermissionDenied()
+    return object
 
 class AnswerCreateView(CreateView):
   model = Answer
@@ -61,6 +74,9 @@ class AnswerCreateView(CreateView):
     return self.object.question.get_absolute_url()
 
   def form_valid(self, form):
+    question = Question.objects.get(id=self.kwargs['pk'])
+    if Answer.objects.filter(question=question, user=self.request.user).exists():
+      raise PermissionDenied()
     form.instance.user = self.request.user
     form.instance.question = Question.objects.get(id=self.kwargs['pk'])
     return super(AnswerCreateView, self).form_valid(form)
@@ -70,6 +86,13 @@ class AnswerUpdateView(UpdateView):
   pk_url_kwarg = 'answer_pk'
   template_name = 'answer/answer_form.html'
   fields = ['text']
+
+  def get_object(self, *args, **kwargs):
+    object = super(AnswerUpdateView, self).get_object(*args, **kwargs)
+    if object.user != self.request.user:
+      raise PermissionDenied()
+    return object
+
 
   def get_success_url(self):
     return self.object.question.get_absolute_url()
@@ -81,4 +104,10 @@ class AnswerDeleteView(DeleteView):
 
   def get_success_url(self):
     return self.object.question.get_absolute_url()
+
+  def get_object(self, *args, **kwargs):
+    object = super(AnswerDeleteView, self).get_object(*args, **kwargs)
+    if object.user != self.request.user:
+      raise PermissionDenied()
+    return object
 
